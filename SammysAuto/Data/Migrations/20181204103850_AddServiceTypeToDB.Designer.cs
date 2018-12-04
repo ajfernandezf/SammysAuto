42b7726d651aaf8855823caa32c3d87e517d58b3
ajfernandezf <ajfernandezf@AJFERNANDEZF10A>
2018-12-04 14:45:09 +0100

    Seccion 5, clase 43

--- SammysAuto/Data/Migrations/20181204103850_AddServiceTypeToDB.Designer.cs
+++ SammysAuto/Data/Migrations/20181204103850_AddServiceTypeToDB.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SammysAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181204103850_AddServiceTypeToDB")]
+    partial class AddServiceTypeToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -184,12 +185,11 @@ namespace SammysAuto.Data.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Name")
-                        .IsRequired();
+                    b.Property<string>("Name");
 
                     b.HasKey("Id");
 
-                    b.ToTable("ServiceTypes");
+                    b.ToTable("serviceTypes");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
---
+++ SammysAuto/Data/Migrations/20181204104315_AddServiceTypeToDB2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SammysAuto.Data.Migrations
+{
+    public partial class AddServiceTypeToDB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_serviceTypes",
+                table: "serviceTypes");
+
+            migrationBuilder.RenameTable(
+                name: "serviceTypes",
+                newName: "ServiceTypes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ServiceTypes",
+                table: "ServiceTypes",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ServiceTypes",
+                table: "ServiceTypes");
+
+            migrationBuilder.RenameTable(
+                name: "ServiceTypes",
+                newName: "serviceTypes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_serviceTypes",
+                table: "serviceTypes",
+                column: "Id");
+        }
+    }
+}
